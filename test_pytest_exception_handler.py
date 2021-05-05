391282ec9add558abb3a37ba085cd0fd10aeed4d
MrRezoo <rezam578@gmail.com>
2021-05-06 02:32:14 +0430

    pytest add Exception handler

---
+++ test_pytest_exception_handler.py
@@ -0,0 +1,27 @@
+import pytest
+import sample_func
+
+
+def test_add():
+    assert sample_func.add(3, 4) == 7
+    assert sample_func.add(-1, 4) == 3
+    assert sample_func.add(0, 4) == 4
+
+
+def subtract():
+    assert sample_func.subtract(3, 4) == -1
+    assert sample_func.subtract(-1, 4) == -5
+    assert sample_func.subtract(0, 4) == -4
+
+
+def multiply():
+    assert sample_func.multiply(3, 4) == 12
+    assert sample_func.multiply(3, 4) != 11
+    assert sample_func.multiply(-1, 4) == -4
+    assert sample_func.multiply(0, 4) == 0
+
+
+def division():
+    assert sample_func.multiply(12, 4) == 3
+    with pytest.raises(ZeroDivisionError):
+        sample_func.division(2, 0)
