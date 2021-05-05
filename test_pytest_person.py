5df36f595ef46845696f6a668eea23d649fb2284
MrRezoo <rezam578@gmail.com>
2021-05-06 02:06:17 +0430

    pytest fixture builded

---
+++ test_pytest_person.py
@@ -0,0 +1,23 @@
+import time
+
+import pytest
+
+from person import Person
+
+
+class TestPerson:
+
+    @pytest.fixture
+    def setup(self):
+        self.p1 = Person('rezoo', 'mob')
+        self.p2 = Person('mik', 'raz')
+        yield 'setup'
+        time.sleep(2)
+
+    def test_fullname(self, setup):
+        assert self.p1.fullname() == 'rezoo mob'
+        assert self.p2.fullname() == 'mik raz'
+
+    def test_email(self, setup):
+        assert self.p1.email() == 'rezoomob@email.com'
+        assert self.p2.email() == 'mikraz@email.com'
