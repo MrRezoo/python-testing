8b1e22039e31f3ce28111a5e69201b25be6d57ac
MrRezoo <rezam578@gmail.com>
2021-05-06 01:41:31 +0430

    nose test & fixture for unittest (setup - teardown)

---
+++ test_unit_person.py
@@ -0,0 +1,25 @@
+import unittest
+
+from person import Person
+
+
+class PersonTest(unittest.TestCase):
+
+    def setUp(self):
+        self.p1 = Person('rezoo', 'mob')
+        self.p2 = Person('mik', 'raz')
+
+    def tearDown(self) -> None:
+        print('Done. . .')
+
+    def test_fullname(self):
+        self.assertEqual(self.p1.fullname(), 'rezoo mob')
+        self.assertEqual(self.p2.fullname(), 'mik raz')
+
+    def test_email(self):
+        self.assertEqual(self.p1.email(), 'rezoomob@email.com')
+        self.assertEqual(self.p2.email(), 'mikraz@email.com')
+
+
+if __name__ == '__main__':
+    unittest.main()
