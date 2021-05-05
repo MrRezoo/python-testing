2edcc94cede261550ff2dfcf22cb7865f840db74
MrRezoo <rezam578@gmail.com>
2021-05-06 00:25:51 +0430

    write doctest & unittest

---
+++ doctest_sample.py
@@ -0,0 +1,34 @@
+def add(x, y):
+    """
+    >>> add(3, 4)
+    7
+    >>> add(4, -1)
+    3
+    >>> add(0, 5)
+    5
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    >>> subtract(3, 4)
+    -1
+    >>> subtract(4, -1)
+    5
+    >>> subtract(6, 5)
+    1
+    """
+    return x - y
+
+
+def multiply(x, y):
+    """
+      >>> multiply(2, 4)
+      8
+      >>> multiply(4, -1)
+      -4
+      >>> multiply(2, 'reza')
+      'rezareza'
+    """
+    return x * y
---
+++ test_sample_func.py
@@ -0,0 +1,26 @@
+import unittest
+
+from sample_func import add, subtract, multiply, division
+
+
+class OneTest(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(4, 9), 13)
+        self.assertEqual(add(-3, 9), 6)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(4, 9), -5)
+        self.assertEqual(subtract(-3, 9), -12)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(4, 9), 36)
+        self.assertEqual(multiply(-3, 9), -27)
+
+    def test_division(self):
+        self.assertEqual(round(division(4, 9)), 0)
+        self.assertEqual(division(18, 9), 2)
+        self.assertRaises(ZeroDivisionError, division, 4, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
